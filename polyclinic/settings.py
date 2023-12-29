94f343c4d2ccaf385bae1decc08d8b0f861f6736
Oleg Markov <onmarkov@yandex.ru>
2023-12-29 14:01:33 +0600

    Update settings.py for Bower

--- polyclinic/settings.py
+++ polyclinic/settings.py
@@ -47,8 +47,7 @@ INSTALLED_APPS = [
     'django_tables2',
     'django_filters',
     'bootstrap3',
-    'registry',
-    'djangobower'
+    'registry'
 ]
 
 MIDDLEWARE = [
@@ -88,13 +87,6 @@ DJANGO_TABLES2_TEMPLATE = 'django_tables2/bootstrap.html'
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 BOWER_COMPONENTS_ROOT = Path.joinpath(BASE_DIR, 'bower_components')
-BOWER_INSTALLED_APPS = (
-    'd3#3.3.13',
-    'nvd3#1.8.6',
-)
-STATICFILES_FINDERS = (
-    'djangobower.finders.BowerFinder',
-)
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
